c082cd213f1775a7756133d9b1c1d9013c6c6e7a
marceloungaretti <marcelo.ungaretti@gmail.com>
2023-04-02 01:59:40 -0300

    fim da aula 29

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -37,16 +37,15 @@ export class CourseComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit() {
-    const searchLessons$ = fromEvent<KeyboardEvent>(this.input.nativeElement, 'keyup')
+    this.lessons$  = fromEvent<KeyboardEvent>(this.input.nativeElement, 'keyup')
       .pipe(
         map((event) => (event.target as HTMLInputElement).value),
+        startWith(''),
         debounceTime(400),
         distinctUntilChanged(),
         switchMap(search => this.loadLessons(search))
       );
 
-    const initialLessons$ = this.loadLessons();
-    this.lessons$ = concat(initialLessons$, searchLessons$)
   }
 
   loadLessons(search: string = ''): Observable<Lesson[]> {
